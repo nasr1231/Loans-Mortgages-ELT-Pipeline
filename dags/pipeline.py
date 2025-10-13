2af2c1031f5f361e64672d4feb96528d5f6d201d
Mohamed Nasr <81034955+nasr1231@users.noreply.github.com>
2025-10-13 03:40:51 +0300

    Add Spark Task In Airflow

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -1,9 +1,6 @@
-from airflow.decorators import dag, task
-from airflow.operators.python import PythonOperator
+from airflow.decorators import dag
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
-# from tasks.jobs import *
-import logging
 
 default_args = {
     "owner": "Mohamed Nasr",
@@ -17,68 +14,28 @@ default_args = {
     schedule_interval='@daily',
     catchup=False,
     default_args=default_args,
-    tags=["stock_market"]
+    tags=["ELT", "Finance"]
 )
 def Loans_ELT_Pipeline():    
-
-    fetch_task = PythonOperator(
-        task_id='get_stock_prices',
-        python_callable = fetch_stock_data,
-        provide_context=True        
-    )
     
     # Task 4: Create Hive table
     create_hive_tables = BashOperator(
         task_id='Create_Hive_Fact_Dimensions',
-        bash_command="""
-        cat > /tmp/create_table.sql << 'SQL'
-CREATE TABLE IF NOT EXISTS real_estate_sales (
-    SaleID INT,
-    List_Year INT,
-    Town STRING,
-    Address STRING,
-    Assessed_Value DOUBLE,
-    Sale_Amount DOUBLE,
-    Sales_Ratio DOUBLE,
-    Property_Type STRING
-)
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
-STORED AS TEXTFILE
-LOCATION '/user/hive/warehouse/real_estate/'
-TBLPROPERTIES ('skip.header.line.count'='1');
-SQL
-
-        docker cp /tmp/create_table.sql hiveserver2:/tmp/create_table.sql
-        docker exec hiveserver2 /opt/hive/bin/beeline \
-            -u jdbc:hive2://localhost:10000 \
-            -f /tmp/create_table.sql --silent=true
+        bash_command="""            
+        docker exec -i ELT_Loan_hive-server bash -c 'hive -f /etc/Hive-DWH-DDL.sql'
         echo "Hive table created successfully"
         """
     )
     
     # Task 5: Load data into Hive
     spark_transform_data = BashOperator(
-        task_id='Load_Hive_Table',
-        bash_command="""
-        cat > /tmp/load_data.sql << 'SQL'
-    LOAD DATA INPATH '/user/hive/warehouse/real_estate/real_estate_sales.csv' 
-    INTO TABLE real_estate_sales;
-
-    SELECT COUNT(*) as total_rows FROM real_estate_sales;
-    SQL
-
-            docker cp /tmp/load_data.sql hiveserver2:/tmp/load_data.sql
-            docker exec hiveserver2 /opt/hive/bin/beeline \
-                -u jdbc:hive2://localhost:10000 \
-                -f /tmp/load_data.sql
-            echo "Data loaded into Hive table successfully"
+        task_id='transform_data',
+        bash_command="""        
+            docker exec -i ELT_Loan_namenode bash -c 'spark-submit --master spark://namenode:7077 /root/airflow/dags/spark.py' 
+            echo "Data has been transformed and loaded into Data warehouse successfully!"
             """
         )
-    
-    
-    
-    fetch_task >> create_hive_tables >> spark_transform_data
-
+        
+    create_hive_tables >> spark_transform_data
 
 Loans_ELT_Pipeline()
